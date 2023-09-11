97d91f746833c660aeb99fd0fc8c39ccb2376b18
Amelss <ameleyka@gmail.com>
2023-09-11 19:43:22 +0100

    Adding mobile image

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,8 @@
 export default function Hero() {
   return (
       <div>
-         <img src="./src/assets/poster.png" alt="hero" />
+          <img src="./src/assets/poster.png" alt="hero" className="hidden md:block" />
+          <img src="./src/assets/mobile-poster.png" alt="mobile-hero" className="block md:hidden" />
           
     </div>
   )
