95b0334022bc89c01b97bf9f42f633eb3f9b2623
Amelss <ameleyka@gmail.com>
2023-09-13 12:34:37 +0100

    Adding writers and directors

--- src/Components/MoviePage.jsx
+++ src/Components/MoviePage.jsx
@@ -9,7 +9,7 @@ export default function MoviePage() {
 
   const details = {
     method: "GET",
-    url: `https://api.themoviedb.org/3/movie/${movieId}`,
+    url: `https://api.themoviedb.org/3/movie/${movieId}?append_to_response=credits`,
     params: { language: "en-US" },
     headers: {
       accept: "application/json",
@@ -24,6 +24,7 @@ export default function MoviePage() {
       const response = await axios.request(details);
       setMovieInfo(response.data);
       setLoading(false);
+      console.log(response.data);
     } catch (error) {
       console.error(error);
       setLoading(false);
@@ -33,6 +34,10 @@ export default function MoviePage() {
   useEffect(() => {
     movieDetails();
   }, [movieId]);
+
+  //     const filteredCast = movieInfo.filter(
+  //       (item) => item.type === "Writing"
+  //   )
 
   return (
     <div>
@@ -65,22 +70,29 @@ export default function MoviePage() {
             <div className="grid grid-cols-1 md:grid-cols-[70%_30%] gap-4 ">
               <div>
                 <div className="block md:flex items-center mt-5 ">
-                  <h2 data-testid="movie-title" className="text-2xl tracking-wide">
+                  <h2
+                    data-testid="movie-title"
+                    className="text-2xl tracking-wide"
+                  >
                     {movieInfo.title}
                   </h2>
                   <span className="hidden md:flex md:ml-3">•</span>
-                  <p data-testid="movie-release-date" className="md:ml-5 text-2xl">
+                  <p
+                    data-testid="movie-release-date"
+                    className="md:ml-5 text-2xl"
+                  >
                     {movieInfo.release_date}
                   </p>
                   <span className="hidden md:flex ml-3 mr-3">•</span>
-                  <p data-testid="movie-runtime" className="text-2xl">{movieInfo.runtime}</p>
+                  <p data-testid="movie-runtime" className="text-2xl">
+                    {movieInfo.runtime}
+                  </p>
                   <span className="hidden md:flex md:ml-3">•</span>
 
                   {movieInfo.genres.slice(0, 2).map((genre, index) => (
                     <div key={index}>
                       <div className="">
                         <p className="md:ml-2 w-max px-2  text-sm border border-movieRed rounded-full md:px-2 border-opacity-25 md:text-base text-movieRed my-2">
-                      
                           {genre.name}
                         </p>
                       </div>
@@ -88,15 +100,65 @@ export default function MoviePage() {
                   ))}
                 </div>
                 <p data-testid="movie-overview">{movieInfo.overview}</p>
+                <div className="block md:flex items-center mt-2 ">
+                  <h1 className="text-xl">Director:</h1>
+                  {[
+                    movieInfo.credits.cast
+                      .filter(
+                        (person) => person.known_for_department === "Directing"
+                      )
+                      .map((director) => director.name)
+                      .slice(0, 1),
+                    ...movieInfo.credits.crew
+                      .filter(
+                        (person) => person.known_for_department === "Directing"
+                      )
+                      .map((director) => director.name)
+                      .slice(0, 1),
+                  ].map((directors, i) => (
+                    <div key={i}>
+                      <p className="md:ml-2 text-movieRed text-lg"> {directors}</p>
+                    </div>
+                  ))}
+                </div>
+                <div className="block md:flex items-center mt-2">
+                  <h1 className=" text-xl">Writer:</h1>
+                  {movieInfo.credits.crew
+                    .filter(
+                      (person) => person.known_for_department === "Writing"
+                    )
+                    .slice(0, 1)
+                    .map((writer, i) => (
+                      <div key={i}>
+                        <p className="text-movieRed text-lg md:ml-2"> {writer.name}</p>
+                      </div>
+                    ))}
+                </div>
+                                  <div className="block md:flex items-center mt-2">
+                                      <div>
+                                          <h1 className="text-xl">Stars:</h1> 
+                                      </div>
+                 
+                  {movieInfo.credits.cast.slice(0, 3).map((credit, index) => (
+                    <div key={index}>
+                      <p className="md:ml-2 text-lg text-movieRed">{credit.name}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
               <div>
-                                  <button className="flex items-center justify-center bg-movieRed rounded-lg px-3 py-3 w-full mb-2">
-                                      <img src="/src/assets/tickets.png" alt="ticket" className="w-5"/>
-                                      <p className="ml-3 text-white">See Showtimes</p></button>
-                                  <button className="flex items-center justify-center bg-movieRed bg-opacity-25 rounded-lg px-3 py-3 w-full border-movieRed">
-                                      <img src="/src/assets/list.png" alt="list" className="w-5"/>
-                                     <p className="ml-3 text-black">More watch options</p> 
-                                  </button>
+                <button className="flex items-center justify-center bg-movieRed rounded-lg px-3 py-3 w-full mb-2">
+                  <img
+                    src="/src/assets/tickets.png"
+                    alt="ticket"
+                    className="w-5"
+                  />
+                  <p className="ml-3 text-white">See Showtimes</p>
+                </button>
+                <button className="flex items-center justify-center bg-movieRed bg-opacity-25 rounded-lg px-3 py-3 w-full border-movieRed">
+                  <img src="/src/assets/list.png" alt="list" className="w-5" />
+                  <p className="ml-3 text-black">More watch options</p>
+                </button>
               </div>
             </div>
           </div>
